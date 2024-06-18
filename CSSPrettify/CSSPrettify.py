74db114ef658769bf2d7ccaec040b64cfd91c3aa
Spark4444 <tstsilin.youtuba@gmail.com>
2024-06-18 22:52:48 +0200

    1.3.0

--- CSSPrettify/CSSPrettify.py
+++ CSSPrettify/CSSPrettify.py
@@ -3,8 +3,8 @@ import os
 import pyperclip
 import re
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import font
+from tkinter import filedialog, font
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
 # Function to edit the CSS code
 def editCss(code, checkState):
@@ -61,19 +61,19 @@ def editCss(code, checkState):
 
     return cssStructure
 
-# Function for opening CSS files
+# Function for opening a CSS files and then editing it
 def openCssFile(checkState, text3, entry2):
-    filepath = filedialog.askopenfilename(filetypes=[("CSS Files", "*.css")])
-    if filepath:
-        with open(filepath, "r") as file:
+    filePath = filedialog.askopenfilename(filetypes=[("CSS Files", "*.css")])
+    if filePath:
+        with open(filePath, "r") as file:
             # Read and save the new CSS file
             cssContent = file.read()
 
-            # get the CSS structure from the editCss function
+            # get the new CSS structure from the editCss function
             cssStructure = editCss(cssContent, checkState)
             
         # Save the new CSS file in the same directory with an "Edited" added at the end
-        newFilename = f"{os.path.splitext(filepath)[0]}Edited.css"
+        newFilename = f"{os.path.splitext(filePath)[0]}Edited.css"
         with open(newFilename, "w") as file:
             file.write(cssStructure)
             text3.config(text="Edited CSS file saved to:")
@@ -85,13 +85,17 @@ def openCssFile(checkState, text3, entry2):
             text3.place(anchor="s", relx=0.5, rely=0.95)
             entry2.place(anchor="s", relx=0.5, rely=1)
 
-# Function for copying the CSS code
-def copyToClipboard(entry, checkState, text3, entry2, root):
+# Function for editing CSS from an entry and then copying it into clipboard
+def copyCssToClipboard(checkState, entry, entry2, event, root, text3):
     cssCode = entry.get()
 
-    # get the CSS structure from the editCss function
+    # get the new CSS structure from the editCss function
     cssStructure = editCss(cssCode, checkState)
+
+    # Copy the new CSS into clipboard
     pyperclip.copy(cssStructure)
+
+    # Set the text3 to display that you"ve copied the new CSS text
     text3.config(text="Copied your new CSS to clipboard.")
     text3.place(anchor="s", relx=0.5, rely=1)
     entry2.config(state="normal")
@@ -101,27 +105,62 @@ def copyToClipboard(entry, checkState, text3, entry2, root):
     entry.delete(0, tk.END) 
     root.focus()
 
-# Function to defocus the second entry
-def defocus(event, entry, entry2):
+# Function for editing a CSS file by dropping it
+def dropCss(checkState, entry2, event, text3, text4):
+    # Assuming only one file is dropped, get the file path
+    filePath = event.data.replace("{", "").replace("}", "").strip()
+    text4.place_forget() 
+    
+    # Open and read the file content
+    with open(filePath, "r") as file:
+        # Read and save the new CSS file
+        cssContent = file.read()
+
+        # get the new CSS structure from the editCss function
+        cssStructure = editCss(cssContent, checkState)
+        
+    # Save the new CSS file in the same directory with an "Edited" added at the end
+    newFilename = f"{os.path.splitext(filePath)[0]}Edited.css"
+    with open(newFilename, "w") as file:
+        file.write(cssStructure)
+        text3.config(text="Edited CSS file saved to:")
+        text3.place(anchor="s", relx=0.5, rely=0.95)
+        entry2.config(state="normal")
+        entry2.delete(0, tk.END)
+        entry2.insert(0, newFilename)
+        entry2.config(state="readonly", cursor="xterm")
+        text3.place(anchor="s", relx=0.5, rely=0.95)
+        entry2.place(anchor="s", relx=0.5, rely=1)
+
+# Function to defocus the second entry if another element is being clicked
+def defocusEntry2(entry, entry2, event):
     if event.widget != entry:
         entry2.focus() 
 
-# Function to change the text of the entry to nothing if it gets clicked
-def onEntryClick(entry, defaultText):
+# Function to change the text of the entry to nothing
+def entryDefaultON(defaultText, entry, event):
     if entry.get() == defaultText:
         entry.delete(0, tk.END)
         entry.insert(0, "")
         entry.config(fg="white")
 
-# Function to set the entry to deafult text if it loses focus
-def onFocusout(entry, defaultText):
+# Function to set the entry to default text
+def entryDefaultOFF(defaultText, entry, event):
     if entry.get() == "":
         entry.insert(0, defaultText)
         entry.config(fg="grey")
 
+# Function to show text4
+def showText4(event, text4):
+    text4.place(relwidth=1, relheight=1)
+
+# Function to hide text4
+def hideText4(event, text4):
+    text4.place_forget() 
+
 def main():
     # Window initalization
-    root = tk.Tk()
+    root = TkinterDnD.Tk()
     root.configure(bg="black")
     root.title("CSS Prettify")
     root.iconbitmap(r"CSSPrettify\dist\main\img\cssIcon.ico")
@@ -134,37 +173,43 @@ def main():
     defaultText = "Copy your CSS here"
 
     # UI initalization
-    customFont = font.Font(family="Helvetica", size=10)
+    normalFont = font.Font(family="Helvetica", size=10)
+    bigFont = font.Font(family="Helvetica", size=40)
     borderFrame = tk.Frame(root, background="white", bd=1, height=30 , width=115)
     
-    text = tk.Label(root, bg="black", fg="white", font=customFont, text="Note that this prettifier removes all comments from your CSS.")
+    text = tk.Label(root, bg="black", fg="white", font=normalFont, text="Note that this prettifier removes all comments from your CSS.\n (You can drop your files and it will work too)")
     text.place(anchor="n", relx=0.5)
 
-    openButton = tk.Button(borderFrame, activebackground="white", activeforeground="black", bg="black", command=lambda: openCssFile(checkState, text3, entry2), cursor="hand2", fg="white", font=customFont, highlightthickness=0, relief="flat", text="Select a CSS file")
+    openButton = tk.Button(borderFrame, activebackground="white", activeforeground="black", bg="black", command=lambda: openCssFile(checkState, text3, entry2), cursor="hand2", fg="white", font=normalFont, highlightthickness=0, relief="flat", text="Select a CSS file")
     borderFrame.place(anchor="center", relx=0.5, rely=0.35)
     openButton.place(anchor="center", relx=0.5, rely=0.45)
 
-    text2 = tk.Label(root, bg="black", fg="white", font=customFont, text="OR")
+    text2 = tk.Label(root, bg="black", fg="white", font=normalFont, text="OR")
     text2.place(anchor="center", relx=0.5, rely=0.5)
 
-    entry = tk.Entry(root, bg="black", bd=0, fg="grey", font=customFont, insertbackground="white", justify="center", width=40)
+    entry = tk.Entry(root, bg="black", bd=0, fg="grey", font=normalFont, insertbackground="white", justify="center", width=40)
     entry.insert(0, defaultText)
     entry.place(anchor="center", relx=0.5, rely=0.60)
 
     checkBox = tk.Checkbutton(root, activebackground="black", activeforeground="white", bg="black", cursor="hand2", fg="white", selectcolor="black", text="Sort element names in alphabetical order", variable=checkState)
     checkBox.place(anchor="center", relx=0.5, rely=0.68)
 
-    text3 = tk.Label(root, bg="black", fg="white", font=customFont, text="")
-    text3.place(anchor="s", relx=0.5, rely=0.95)
+    text3 = tk.Label(root, bg="black", fg="white", font=normalFont, text="")
 
-    entry2 = tk.Entry(root, bd=0, cursor="arrow", fg="white", font=customFont, justify="center", readonlybackground="black", state="readonly", width=65)
+    entry2 = tk.Entry(root, bd=0, cursor="arrow", fg="white", font=normalFont, justify="center", readonlybackground="black", state="readonly", width=65)
     entry2.place(anchor="s", relx=0.5, rely=1)
 
+    text4 = tk.Label(root, bg="black", fg="white", font=bigFont, text="Drop your file here")
+
     # Binds
-    root.bind("<1>", lambda event: defocus(event, entry, entry2))
-    entry.bind("<Return>", lambda event: copyToClipboard(entry, checkState, text3, entry2, root))
-    entry.bind("<FocusIn>", lambda event: onEntryClick(entry, defaultText))
-    entry.bind("<FocusOut>", lambda event: onFocusout(entry, defaultText))
+    entry.bind("<FocusIn>", lambda event: entryDefaultON(defaultText, entry, event))
+    entry.bind("<FocusOut>", lambda event: entryDefaultOFF(defaultText, entry, event))
+    entry.bind("<Return>", lambda event: copyCssToClipboard(checkState, entry, entry2, event, root, text3))
+    root.bind("<1>", lambda event: defocusEntry2(entry, entry2, event))
+    root.dnd_bind("<<Drop>>", lambda event: dropCss(checkState, entry2, event, text3, text4))
+    root.dnd_bind("<<DropEnter>>", lambda event: showText4(event, text4))
+    root.dnd_bind("<<DropLeave>>", lambda event: hideText4(event, text4))
+    root.drop_target_register(DND_FILES)
 
     root.mainloop()
 
